ae7c3cb929d1a79e2a2c2e2a5bdf54738d5fd4c0
Giorgi Krialashvili <Giokriala@hotmail.com>
2020-11-03 23:56:15 +0400

    gkialashvili

---
+++ src/components/AboutPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AboutPage() {
+  return (
+    <React.Fragment>
+      <h2>About</h2>
+      <p>Thos app uses React.</p>
+    </React.Fragment>
+  );
+}
+
+export default AboutPage;
---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="jumbotron">
+      <h1>Pluralsight</h1>
+      <p>React, flux, and React Router</p>
+    </div>
+  );
+}
+
+export default HomePage;
